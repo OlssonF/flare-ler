c42fbbbe827c7079e34626079f38fed802a84431
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2022-09-20 12:13:07 -0400

    scoring the combined empirical forecast. Use pretend data for climatology forecast

--- ler_baseline_forecasts.R
+++ ler_baseline_forecasts.R
@@ -10,52 +10,56 @@ targets <- read_csv('https://s3.flare-forecast.org/targets/ler/fcre/fcre-targets
   # mutate(site_id = paste(site_id, depth, sep = '_'))
 
 # When to produce forecasts for
-forecast_dates <- seq(ymd('2020-11-08'), ymd('2021-03-01'), 7)
+forecast_dates <- seq(ymd('2018-08-03'), ymd('2019-01-11'), 7)
 
 forecast_vars <- expand.grid(start = forecast_dates, depth_use = unique(targets$depth)) %>%
   mutate(#targets = 'targets',
-         h = 34)
+         h = 14)
 
 ##### 1. Random walk ####
-forecast.RW  <- function(start, h= 34, depth_use) {
+forecast.RW  <- function(start, h= 14, depth_use) {
   
   # Work out when the forecast should start
   forecast_starts <- targets %>%
-    dplyr::filter(!is.na(observed) & depth == depth_use, time < start) %>%
-    # Start the day after the most recent non-NA value
-    dplyr::summarise(start_date = ymd(max(time) + lubridate::days(1))) %>% # Date
-    dplyr::mutate(h = (start - start_date) + h) %>% # Horizon value
-    dplyr::ungroup()
+    dplyr::filter(!is.na(observed) & depth == depth_use, time < start)
   
-  # Generate the RW model
-  RW_model <- targets %>%
-    mutate(time = as_date(time)) %>%
-    # filter the targets data for the depth and start time
-    dplyr::filter(depth == depth_use & time < start) %>%
-    tsibble::as_tsibble(key = 'depth', index = 'time') %>%
-    # add NA values
-    tsibble::fill_gaps() %>%
-    # Remove the NA's put at the end, so that the forecast starts from the last day with an observation,
-    # rather than today
-    dplyr::filter(time < forecast_starts$start_date)  %>%
-    fabletools::model(RW = fable::RW(observed))
-  
-  # Generate the forecast
-  RW_forecast <- RW_model %>%
-    fabletools::generate(h = as.numeric(forecast_starts$h),
-                         bootstrap = T,
-                         times = 200) %>%
-    rename(model_id = .model,
-           predicted = .sim,
-           ensemble = .rep) %>%
-    as_tibble() %>% 
-    mutate(#h = as.numeric(time - min(time) + 1),
-           start_time = start) 
-
-  message('RW forecast for ', start, ' at ', depth_use, ' m')
-  return(RW_forecast)
-  
-  
+  if (nrow(forecast_starts) !=0) {
+    forecast_starts <- forecast_starts %>% 
+      # Start the day after the most recent non-NA value
+      dplyr::summarise(start_date = ymd(max(time) + lubridate::days(1))) %>% # Date
+      dplyr::mutate(h = (start - start_date) + h) %>% # Horizon value
+      dplyr::ungroup()
+    
+    # Generate the RW model
+    RW_model <- targets %>%
+      mutate(time = as_date(time)) %>%
+      # filter the targets data for the depth and start time
+      dplyr::filter(depth == depth_use & time < start) %>%
+      tsibble::as_tsibble(key = 'depth', index = 'time') %>%
+      # add NA values
+      tsibble::fill_gaps() %>%
+      # Remove the NA's put at the end, so that the forecast starts from the last day with an observation,
+      # rather than today
+      dplyr::filter(time < forecast_starts$start_date)  %>%
+      fabletools::model(RW = fable::RW(observed))
+    
+    # Generate the forecast
+    RW_forecast <- RW_model %>%
+      fabletools::generate(h = as.numeric(forecast_starts$h),
+                           bootstrap = T,
+                           times = 200) %>%
+      rename(model_id = .model,
+             predicted = .sim,
+             ensemble = .rep) %>%
+      as_tibble() %>% 
+      mutate(#h = as.numeric(time - min(time) + 1),
+        start_time = start) 
+    
+    message('RW forecast for ', start, ' at ', depth_use, ' m')
+    return(RW_forecast)
+  }  else {
+    message('RW forecast not run for ', start, ' at ', depth_use, ' m')
+  }
 }
 
 
@@ -70,16 +74,17 @@ write_csv(RW, 'RW.csv.gz')
 
 ##### 2. Climatology ####
 
-forecast.clim <- function(targets = targets, start, h=34) {
+forecast.clim <- function(targets = targets, start, h=14) {
   # only the targets available before should be used to produce the forecast
+  # currently uses all data
   doy_targets <- targets %>%
-    filter(time < start) %>%
+    # filter(time < start) %>%
     # this gives the day of year as if it were a leap year
     mutate(doy = ifelse(yday(time) > 59 & leap_year(time) == F,
                         yday(time) + 1,
                         yday(time))) %>%
     # find the day of year average
-    group_by(depth, doy) %>%
+    group_by(doy, depth) %>%
     summarise(predicted = mean(observed))
   
   # Day of year of the forecast dates
@@ -102,7 +107,7 @@ forecast.clim <- function(targets = targets, start, h=34) {
     # Uncertainty - Calculate the sd between observations of each month per depth and then find the 
     # average (mean and median) of these, gives a different sd for each depth
   clim_forecast <- targets %>%
-    filter(time < start) %>%
+    # filter(time < start) %>%
     mutate(month = zoo::as.yearmon(time)) %>%
     group_by(month, depth) %>% 
     # Find the standard deviation of observations for each month
@@ -125,12 +130,58 @@ forecast.clim <- function(targets = targets, start, h=34) {
   
 }
 
-climatology <- forecast_dates %>%
-  map_dfr( ~ forecast.clim(targets = targets, start = .x)) %>%
-  mutate(site_id = 'fcre',
+# pretend data that we can use to develop method
+forecast_dates_clim <- forecast_dates + years(1)
+
+# get the climatology forecast for each date
+climatology <- forecast_dates_clim %>%
+  map_dfr( ~ forecast.clim(targets = subset(targets, time > max(forecast_dates_clim)), start = .x)) #%>%
+
+# Function to create an ensemble from the mean and standard deviation
+create.ensemble <- function(climatology, times) {
+  data.frame(ensemble = 1:200, predicted = rnorm(n=200, mean = climatology$predicted, sd= climatology$sd)) %>%
+    mutate(time = climatology$time, 
+           start_time = climatology$start_time,
+           depth = climatology$depth)
+}
+
+# Run the function over each row (time, start_time and depth combination)
+climatology_ensemble <- climatology %>%
+  split(1:nrow(.)) %>%
+  purrr::map_dfr(create.ensemble) %>%
+  # add in the extra columns needed
+  mutate(model_id = 'climatology',
+         site_id = 'fcre',
          variable = 'temperature',
          forecast = 0,
-         variable_type = 'state')
+         variable_type = 'state',
+         # pretend data
+         time = time - years(1),
+         start_time = start_time - years(1))
 
-write_csv(climatology, 'climatology.csv.gz') 
+write_csv(climatology_ensemble, 'climatology.csv.gz') 
 #================================#
+
+# Combine and score the two empirical forecasts
+
+# Scoring forecasts individually
+score_file_RW <- FLAREr::generate_forecast_score(
+  # don't need to read in targets before
+  targets_file = 'https://s3.flare-forecast.org/targets/ler/fcre/fcre-targets-insitu.csv',
+  # needs to be from the csv
+  forecast_file = 'RW.csv.gz',
+  output_directory = './scores')
+
+scored_RW <- read_parquet(score_file_RW)
+
+
+
+score_file_climatology <- FLAREr::generate_forecast_score(
+  # don't need to read in targets before
+  targets_file = 'https://s3.flare-forecast.org/targets/ler/fcre/fcre-targets-insitu.csv',
+  # needs to be from the csv
+  forecast_file = 'climatology.csv.gz',
+  output_directory = './scores')
+
+
+scored_climatology <- arrow::read_parquet(score_file_climatology)
