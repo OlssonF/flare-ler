585f3034b6a3dcc71bf895714961ae66ace4c2bf
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2022-11-14 12:57:53 -0500

    custom scoring function to work with LER output

---
+++ R/scoring_function.R
@@ -0,0 +1,35 @@
+generate_forecast_score_2 <- function(targets_file,
+                                    forecast_file,
+                                    output_directory){
+  
+  target <- readr::read_csv(targets_file, show_col_types = FALSE) |>
+    dplyr::mutate(site_id = paste0(site_id,"-",depth))
+  
+  if("time" %in% colnames(target)){
+    target <- target |>
+      dplyr::rename(datetime = time)
+  }
+  
+  fn <- tools::file_path_sans_ext(tools::file_path_sans_ext(basename(forecast_file)))
+  
+  file_name <- file.path(output_directory,paste0(fn, ".parquet"))
+  
+  forecast <- forecast_file %>%
+    read4cast::read_forecast() %>%
+    select(-any_of(pub_time)) %>%
+    filter(variable_type == "state") %>%
+    dplyr::mutate(filename = forecast_file,
+                  site_id = paste0(site_id,"-",depth)) %>%
+    score4cast::standardize_forecast() %>%
+    dplyr::mutate(family = as.character(family)) %>%
+    score4cast::crps_logs_score(target) %>%
+    
+    mutate(reference_datetime = lubridate::as_datetime(reference_datetime),
+           horizon = datetime-reference_datetime) %>%
+    mutate(horizon = as.numeric(lubridate::as.duration(horizon),
+                                units = "seconds"),
+           horizon = horizon / 86400) %>%
+    arrow::write_parquet(file_name)
+  
+  invisible(file_name)
+}
