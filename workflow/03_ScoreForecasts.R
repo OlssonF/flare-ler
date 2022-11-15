9a540bdc87a6042d23e5f319d8ee68bf3902bbfe
Freya E Olsson <freyao@tc009.cm.cluster>
2022-11-15 13:17:03 -0500

    scoring new parquet files looping over each model-reference_datetime combination

--- workflow/03_ScoreForecasts.R
+++ workflow/03_ScoreForecasts.R
@@ -1,8 +1,7 @@
 # Script 3 in workflow that takes all 8 forecasts and scores them using 
 # the FLAREr function 
 
-library(GLM3r)
-library(FLAREr)
+# library(FLAREr)
 library(arrow)
 library(tidyverse)
 library(lubridate)
@@ -13,24 +12,43 @@ if (getwd() == dirname(rstudioapi::getSourceEditorContext()$path)) {
   setwd('../')
 } 
 
-source('./R/scoring_function.R')
+source('./R/scoring_function_arrow.R')
 
-if (!exists('./scores/')) {
+if (!dir.exists('./scores/')) {
   dir.create('./scores/')
 }
 
 
 #### c) Score forecasts ####
-# Get a list of the forecasts to be scored
-forecast_files <- list.files('./forecasts', pattern = '.gz')
-# forecasts_to_score <- forecast_files[c(1,6,7,8,9,12,13,14)]
+# Open th dataset of forecast parquets to be scored
+forecast_parquets <- './forecasts/site_id=fcre'
+forecast_s3 <- arrow::SubTreeFileSystem$create(forecast_parquets)
+open_parquets <- arrow::open_dataset(forecast_s3) 
 
-for (i in 1:length(forecast_files)) {
-  forecast_file <- paste0('./forecasts/', forecast_files[i])
-  generate_forecast_score_2(targets_file = 'https://s3.flare-forecast.org/targets/ler_ms/fcre/fcre-targets-insitu.csv',
-                                  forecast_file = forecast_file,
-                                  output_directory = './scores')
-  message(forecast_files[i], ' scored')
-}
+# vector of unique model_ids
+models <- open_parquets |>
+  distinct(model_id) |> 
+  collect() |> 
+  pull(model_id)
+# vector of unique reference_Datetimes
+unique_reftime <- open_parquets |>
+  distinct(reference_datetime) |> 
+  collect() |> 
+  pull(reference_datetime)
+model_refdates <- expand.grid(model_id = models, reference_datetime = unique_reftime)
 
+for (i in 1:nrow(model_refdates)) {
+  # subset the model and reference datetime
+  forecast_df <- open_parquets|>
+  dplyr::filter(model_id == model_refdates$model_id[i], 
+                reference_datetime == model_refdates$reference_datetime[i]) |>
+    mutate(site_id = 'fcre') |>
+  collect()
+  
+  # Score the forecast
+  generate_forecast_score_arrow(targets_file = 'https://s3.flare-forecast.org/targets/ler_ms/fcre/fcre-targets-insitu.csv',
+                                forecast_df = forecast_df,
+                                local_directory = './scores')
+  message(i, "/", nrow(model_refdates), " forecasts scored")
+} 
 #=============================#
